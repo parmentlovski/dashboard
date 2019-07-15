abc0210b3ee0af2b40baf0d869648996529b6af1
parmentlovski <parmentelot.bryan@hotmail.fr>
2019-07-15 19:57:01 +0000

    début du graphisme : présence des utilisateurs par évènements

--- admin/graph.php
+++ admin/graph.php
@@ -103,7 +103,7 @@ function soustract()
 
 ?>
 
-    <canvas id="canvas2" class="offset-3 col-9"></canvas>
+    <canvas id="canvas2"></canvas>
 
 <script>
     var barChartData = {
@@ -151,7 +151,8 @@ function soustract()
             options: {
                 title: {
                     display: true,
-                    text: 'Chart.js Bar Chart - Stacked'
+                    fontSize: 35,
+                    text: 'Places disponibles sur les 4 prochains évènements'
                 },
                 tooltips: {
                     mode: 'index',
@@ -171,6 +172,8 @@ function soustract()
     };
 
 </script>
+
+
 </main>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
--- admin/home.php
+++ admin/home.php
@@ -22,259 +22,283 @@ if (isset($_GET['logout'])) {
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js"></script>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<link href="https://fonts.googleapis.com/css?family=Graduate&display=swap" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Graduate&display=swap" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="/assets/css/style.css">
 </head>
 
 <body>
 
-<?php include('../nav.php') ?>
+	<?php include('../nav.php') ?>
 
 	<section class="graphique container-fluid">
 		<h1>Les statistiques du club</h1>
 		<div class="container">
 			<div class="row">
-				<canvas id="myChart" class="offset-3 col-9"> </canvas>
+			<canvas id="myChart" class="offset-3 col-9"> </canvas>
 
-				<?php
+<?php
 
-				$sql_graph = "SELECT places_necessaires FROM planning WHERE places_necessaires";
-				$sth_graph = $db->prepare($sql_graph);
-				$sth_graph->execute();
-				$result_graph = $sth_graph->fetchAll(PDO::FETCH_ASSOC);
+$sql_graph = "SELECT places_necessaires FROM planning WHERE places_necessaires";
+$sth_graph = $db->prepare($sql_graph);
+$sth_graph->execute();
+$result_graph = $sth_graph->fetchAll(PDO::FETCH_ASSOC);
 
-				for ($i1 = 0; $i1 < sizeof($result_graph); $i1++) {
-					$graph = 0;
+for ($i1 = 0; $i1 < sizeof($result_graph); $i1++) {
+    $graph = 0;
 
-					$graph += $result_graph[$i1]["places_necessaires"];
-					echo ($result_graph[$i1]['places_necessaires'] . ',');
-				}
+    $graph += $result_graph[$i1]["places_necessaires"];
+    echo ($result_graph[$i1]['places_necessaires'] . ',');
+}
 
-				// function pourcent()
-				// {
+// function pourcent()
+// {
 
-				//     global $db, $date_event, $result_sco;
+//     global $db, $date_event, $result_sco;
 
-				//     $sql_sco = "SELECT `jour_event`, SUM(`places_reservees`) FROM response_parent GROUP BY `jour_event`";
-				//     $sth_sco = $db->prepare($sql_sco);
-				//     $sth_sco->bindParam(':jour_event', $date_event, PDO::PARAM_STR);
-				//     $sth_sco->execute();
-				//     $result_sco = $sth_sco->fetchAll(PDO::FETCH_ASSOC);
-
-
-				//     $sql1 = "SELECT places_necessaires FROM planning";
-				//     $sthpN = $db->prepare($sql1);
-				//     $sthpN->execute();
-				//     $placeLimite = $sthpN->fetch(PDO::FETCH_ASSOC);
-
-				//     for ($i = 0; $i < sizeof($result_sco); $i++) {
-
-				//         //echo de mes places additionné selon les dates
-				//         echo $result_sco[$i]['SUM(`places_reservees`)'].',';
+//     $sql_sco = "SELECT `jour_event`, SUM(`places_reservees`) FROM response_parent GROUP BY `jour_event`";
+//     $sth_sco = $db->prepare($sql_sco);
+//     $sth_sco->bindParam(':jour_event', $date_event, PDO::PARAM_STR);
+//     $sth_sco->execute();
+//     $result_sco = $sth_sco->fetchAll(PDO::FETCH_ASSOC);
 
 
-				//         //echo du calcul de pourcentage entre le nombre de places prise et le nombre de place necessaire par date
-				//         echo (($result_sco[$i]['SUM(`places`)'] * 100) / $placeLimite['places_necessaires'].',');
-				//     }
+//     $sql1 = "SELECT places_necessaires FROM planning";
+//     $sthpN = $db->prepare($sql1);
+//     $sthpN->execute();
+//     $placeLimite = $sthpN->fetch(PDO::FETCH_ASSOC);
 
-				// }
+//     for ($i = 0; $i < sizeof($result_sco); $i++) {
 
-
-				function addition()
-				{
-
-					global $db, $date_event, $result_sco;
-
-					$sql_sco = "SELECT `jour_event`, SUM(`places_reservees`) FROM response_parent GROUP BY `jour_event`";
-					$sth_sco = $db->prepare($sql_sco);
-					$sth_sco->bindParam(':jour_event', $date_event, PDO::PARAM_STR);
-					$sth_sco->execute();
-					$result_sco = $sth_sco->fetchAll(PDO::FETCH_ASSOC);
+//         //echo de mes places additionné selon les dates
+//         echo $result_sco[$i]['SUM(`places_reservees`)'].',';
 
 
-					$sql1 = "SELECT places_necessaires FROM planning";
-					$sthpN = $db->prepare($sql1);
-					$sthpN->execute();
-					$placeLimite = $sthpN->fetch(PDO::FETCH_ASSOC);
+//         //echo du calcul de pourcentage entre le nombre de places prise et le nombre de place necessaire par date
+//         echo (($result_sco[$i]['SUM(`places`)'] * 100) / $placeLimite['places_necessaires'].',');
+//     }
 
-					for ($i = 0; $i < sizeof($result_sco); $i++) {
-
-						//echo de mes places additionné selon les dates
-						echo $result_sco[$i]['SUM(`places_reservees`)'] . ',';
+// }
 
 
-						//echo du calcul de pourcentage entre le nombre de places prise et le nombre de place necessaire par date
-						echo (($result_sco[$i]['SUM(`places_reservees`)'] * 100) / $placeLimite['places_necessaires'] . ',');
-					}
-				}
+function addition()
+{
 
-				function soustract()
-				{
+    global $db, $date_event, $result_sco;
 
-					global $db, $date_event, $i;
-					$sql_graph = "SELECT places_necessaires FROM planning WHERE places_necessaires";
-					$sth_graph = $db->prepare($sql_graph);
-					$sth_graph->execute();
-					$result_graph = $sth_graph->fetchAll(PDO::FETCH_ASSOC);
-
-					$sql_sco = "SELECT `jour_event`, SUM(`places_reservees`) FROM response_parent GROUP BY `jour_event`";
-					$sth_sco = $db->prepare($sql_sco);
-					$sth_sco->bindParam(':jour_event', $date_event, PDO::PARAM_STR);
-					$sth_sco->execute();
-					$result_sco = $sth_sco->fetchAll(PDO::FETCH_ASSOC);
-
-					for ($i1 = 0; $i1 < sizeof($result_graph); $i1++) {
-						$graph = 0;
-
-						$graph += $result_graph[$i1]["places_necessaires"];
-						echo ($result_graph[$i1]['places_necessaires'] . ',');
-					}
-
-					for ($j = 0; $j < sizeof($result_sco); $j++) {
-						echo $result_sco[$j]['SUM(`places_reservees`)'] . ',';
-						//echo de mes places additionné selon les dates
-						echo $result_sco[$j]['SUM(`places_reservees`)'] . ',';
-					}
-				}
-				soustract()
-
-				?>
-				<script>
-					var ctx = document.getElementById('myChart');
-					var myChart = new Chart(ctx, {
-						type: 'pie',
-						data: {
-							labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-							datasets: [{
-								label: '# of Votes',
-								data: [<?php addition(); ?>],
-								backgroundColor: [
-									'rgba(255, 99, 132, 0.2)',
-									'rgba(54, 162, 235, 0.2)',
-									'rgba(255, 206, 86, 0.2)'
-								],
-								borderColor: [
-									'rgba(255, 99, 132, 1)',
-									'rgba(54, 162, 235, 1)',
-									'rgba(255, 206, 86, 1)'
-
-								],
-								borderWidth: 1
-							}]
-						},
-						options: {
-							scales: {
-								yAxes: [{
-									ticks: {
-										beginAtZero: true
-									}
-								}]
-							}
-						}
-					});
-				</script>
-
-				<canvas id="canvas" class="offset-3 col-9"></canvas>
-
-				<script>
-					var barChartData = {
-						labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-						datasets: [{
-							label: 'Places réservées',
-							data: [<?php addition(); ?>],
-							backgroundColor: [
-								'green',
-								'green',
-								'green',
-								'green',
-								'green',
-								'green',
-								'green'
-
-							],
-							borderColor: [
-								'rgba(255, 99, 132, 1)',
-								'rgba(54, 162, 235, 1)',
-								'rgba(255, 206, 86, 1)',
-								'rgba(75, 192, 192, 1)',
-								'rgba(153, 102, 255, 1)',
-								'rgba(255, 159, 64, 1)',
-								'rgba(255, 159, 64, 1)'
-
-							],
-							borderWidth: 1
-						}, {
-							label: 'Places nécessaires',
-							data: [<?php
-									$graph = 0;
-									for ($i = 0; $i < sizeof($result_graph); $i++) {
-										$graph += $result_graph[$i]["places_necessaires"];
-										echo ($result_graph[$i]['places_necessaires'] . ',');
-									} ?>],
-							backgroundColor: [
-								'red',
-								'red',
-								'red',
-								'red',
-								'red',
-								'red',
-								'red'
-
-							],
-							borderColor: [
-								'rgba(255, 99, 132, 1)',
-								'rgba(54, 162, 235, 1)',
-								'rgba(255, 206, 86, 1)',
-								'rgba(75, 192, 192, 1)',
-								'rgba(153, 102, 255, 1)',
-								'rgba(255, 159, 64, 1)',
-								'rgba(255, 159, 64, 1)'
-							],
-							borderWidth: 1
+    $sql_sco = "SELECT `jour_event`, SUM(`places_reservees`) FROM response_parent GROUP BY `jour_event`";
+    $sth_sco = $db->prepare($sql_sco);
+    $sth_sco->bindParam(':jour_event', $date_event, PDO::PARAM_STR);
+    $sth_sco->execute();
+    $result_sco = $sth_sco->fetchAll(PDO::FETCH_ASSOC);
 
 
+    $sql1 = "SELECT places_necessaires FROM planning";
+    $sthpN = $db->prepare($sql1);
+    $sthpN->execute();
+    $placeLimite = $sthpN->fetch(PDO::FETCH_ASSOC);
 
-						}]
+    for ($i = 0; $i < sizeof($result_sco); $i++) {
 
-					};
-					window.onload = function() {
-						var ctx = document.getElementById('canvas').getContext('2d');
-						window.myBar = new Chart(ctx, {
-							type: 'bar',
-							data: barChartData,
-							options: {
-								title: {
-									display: true,
-									text: 'Chart.js Bar Chart - Stacked'
-								},
-								tooltips: {
-									mode: 'index',
-									intersect: false
-								},
-								responsive: true,
-								scales: {
-									xAxes: [{
-										stacked: true,
-									}],
-									yAxes: [{
-										stacked: true
-									}]
-								}
-							}
-						});
-					};
+        //echo de mes places additionné selon les dates
+        echo $result_sco[$i]['SUM(`places_reservees`)'] . ',';
 
-					document.getElementById('randomizeData').addEventListener('click', function() {
-						barChartData.datasets.forEach(function(dataset) {
-							dataset.data = dataset.data.map(function() {
-								return randomScalingFactor();
-							});
-						});
-						window.myBar.update();
-					});
-				</script>
-			</div>
-		</div>
+
+        //echo du calcul de pourcentage entre le nombre de places prise et le nombre de place necessaire par date
+        echo (($result_sco[$i]['SUM(`places_reservees`)'] * 100) / $placeLimite['places_necessaires'] . ',');
+    }
+}
+
+function soustract()
+{
+
+    global $db, $date_event, $i;
+    $sql_graph = "SELECT places_necessaires FROM planning WHERE places_necessaires";
+    $sth_graph = $db->prepare($sql_graph);
+    $sth_graph->execute();
+    $result_graph = $sth_graph->fetchAll(PDO::FETCH_ASSOC);
+
+    $sql_sco = "SELECT `jour_event`, SUM(`places_reservees`) FROM response_parent GROUP BY `jour_event`";
+    $sth_sco = $db->prepare($sql_sco);
+    $sth_sco->bindParam(':jour_event', $date_event, PDO::PARAM_STR);
+    $sth_sco->execute();
+    $result_sco = $sth_sco->fetchAll(PDO::FETCH_ASSOC);
+
+    for ($i1 = 0; $i1 < sizeof($result_graph); $i1++) {
+        $graph = 0;
+
+        $graph += $result_graph[$i1]["places_necessaires"];
+        echo ($result_graph[$i1]['places_necessaires'] . ',');
+    }
+
+    for ($j = 0; $j < sizeof($result_sco); $j++) {
+        echo $result_sco[$j]['SUM(`places_reservees`)'] . ',';
+        //echo de mes places additionné selon les dates
+        echo $result_sco[$j]['SUM(`places_reservees`)'] . ',';
+    }
+}
+soustract()
+
+?>
+<script>
+    var ctx = document.getElementById('myChart');
+    var myChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [<?php addition(); ?>],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)'
+
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+</script>
+
+<canvas id="canvas" class="offset-3 col-9"></canvas>
+
+<script>
+    var barChartData = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'Places réservées',
+            data: [<?php addition(); ?>],
+            backgroundColor: [
+                'green',
+                'green',
+                'green',
+                'green',
+                'green',
+                'green',
+                'green'
+
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 159, 64, 1)'
+
+            ],
+            borderWidth: 1
+        }, {
+            label: 'Places nécessaires',
+            data: [<?php
+                    $graph = 0;
+                    for ($i = 0; $i < sizeof($result_graph); $i++) {
+                        $graph += $result_graph[$i]["places_necessaires"];
+                        echo ($result_graph[$i]['places_necessaires'] . ',');
+                    } ?>],
+            backgroundColor: [
+                'red',
+                'red',
+                'red',
+                'red',
+                'red',
+                'red',
+                'red'
+
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+
+
+
+        }]
+
+    };
+    window.onload = function() {
+        var ctx = document.getElementById('canvas').getContext('2d');
+        window.myBar = new Chart(ctx, {
+            type: 'bar',
+            data: barChartData,
+            options: {
+                title: {
+                    display: true,
+                    text: 'Chart.js Bar Chart - Stacked'
+                },
+                tooltips: {
+                    mode: 'index',
+                    intersect: false
+                },
+                responsive: true,
+                scales: {
+                    xAxes: [{
+                        stacked: true,
+                    }],
+                    yAxes: [{
+                        stacked: true
+                    }]
+                }
+            }
+        });
+    };
+
+    document.getElementById('randomizeData').addEventListener('click', function() {
+        barChartData.datasets.forEach(function(dataset) {
+            dataset.data = dataset.data.map(function() {
+                return randomScalingFactor();
+            });
+        });
+        window.myBar.update();
+    });
+</script>
+</div>
+</div>
+
+		<!-- <canvas id="userstest" width="100" height="100"></canvas>
+		<script>
+			data = {
+				datasets: [{
+					data: [10, 20, 30]
+				}],
+
+				// These labels appear in the legend and in the tooltips when hovering different arcs
+				labels: [
+					'Red',
+					'Yellow',
+					'Blue'
+				]
+			};
+			window.onload = function() {
+				var ctx = document.getElementById('usertest').getContext('2d');
+				var myPieChart = new Chart(ctx, {
+					type: 'pie',
+					data: data,
+					options: options
+				});
+			};
+		</script> -->
 	</section>
 
 
